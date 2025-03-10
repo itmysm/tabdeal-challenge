697e2590bb6c01f2a442654fbe22313dc3ed65cd
itmysm <dev.mysm@gmail.com>
2025-03-10 22:52:06 +0330

    feat: created gradient-wrapper component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,8 @@ export default defineNuxtConfig({
   },
 
   devtools: { enabled: true },
-  modules: ["@nuxt/image"]
+  modules: ["@nuxt/image"],
+  image: {
+    dir: 'assets/images',
+  },
 })
---
+++ src/components/shared/GradientWrapper.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-linear-90 from-background via-tertiary to-tertiary h-99 relative py-3 pb-8 md:pb-16">
+    <NuxtImg src="/gradient-wrapper.png" alt="Gradient Wrapper" class="absolute top-1/3 -left-1/3 md:top-0 md:w-5/12 md:left-3/12 w-full" />
+    <slot />
+  </div>
+</template>
