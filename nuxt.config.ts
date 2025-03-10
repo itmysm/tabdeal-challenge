1557b74f70eda1e96edc2d433ba00db72488a125
itmysm <dev.mysm@gmail.com>
2025-03-10 20:45:07 +0330

    chore: added tailwindcss to nuxt config and customized theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   srcDir: 'src/',
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
   devtools: { enabled: true }
 })
