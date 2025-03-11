1980d1f54f8a5264fe588a4816703c085b5249dc
itmysm <dev.mysm@gmail.com>
2025-03-11 16:21:26 +0330

    feat: created character card and a skeleton for loading stage

---
+++ src/components/shared/characters/CharacterCardSkeleton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col gap-y-3 animate-pulse p-3">
+    <span
+      class="bg-background-secondary min-w-[296px] min-h-[391px] max-w-[352px] h-full w-full max-h-[469px] rounded-lg"
+    />
+    <span class="bg-background-secondary w-1/2 h-4 rounded-lg" />
+  </div>
+</template>
