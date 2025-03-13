3a8f291cd967fa318ad7aaf1d1aa7a43cd9e99fc
itmysm <dev.mysm@gmail.com>
2025-03-13 14:22:16 +0330

    fix: now images load correctly on production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  srcDir: 'src/',
+  compatibilityDate: "2024-11-01",
+  srcDir: "src/",
   css: ["~/assets/css/main.css"],
 
   vite: {
-    plugins: [
-      tailwindcss()
-    ]
+    plugins: [tailwindcss()],
   },
 
   devtools: { enabled: true },
   modules: ["@nuxt/image"],
   image: {
-    dir: 'assets/images',
+    dir: "assets/images",
+    domains: ['i.annihil.us']
   },
 
   runtimeConfig: {
@@ -23,6 +22,6 @@ export default defineNuxtConfig({
     public: {
       baseUrl: process.env.BASE_URL,
       marvelPublicKey: process.env.MARVEL_PUBLIC_KEY,
-    }
+    },
   },
-})
+});
