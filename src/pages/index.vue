13b94c3b6a883827b53d246cc978fe87165256a3
itmysm <dev.mysm@gmail.com>
2025-04-06 15:14:34 +0330

    refactor: appreciate

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,31 +1,11 @@
 <template>
   <div class="bg-background">
-    <GradientWrapper class="px-4">
-      <Header />
-      <SearchCharacters @onSearch="onHandleSearch" :reset-trigger="resetTrigger" :loading="status === 'pending'" />
-    </GradientWrapper>
-
-    <div class="container mx-auto">
-      <div v-if="characters && characters.characters.length && status === 'success'" class="mt-4">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          <NuxtLink :to="`/${character.id}`" class="flex justify-center p-3" v-for="character in characters.characters"
-            :key="`character-${character.id}`">
-            <ItemCard :item="{ name: character.name, thumbnail: character.thumbnail }" />
-          </NuxtLink>
-        </div>
-      </div>
-
-      <div v-else-if="status === 'pending'"
-        class="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <ItemCardSkeleton v-for="i in limit" :key="`skeleton-${i}`" />
-      </div>
-
-      <div class="mt-12" v-else-if="characters && characters.characters.length === 0">
-        <NoResult @back-to-first-page="resetSearch" />
-      </div>
-
-      <Pagination v-if="characters && characters?.totalPages > 1" class="mt-12" :currentPage="currentPage"
-        :totalPages="characters.totalPages" @change="changePage" />
+    <div class="flex flex-col items-center justify-center w-full h-full">
+      <img class="w-8/12 lg:w-4/12 max-h-[70%]" :src="angry" />
+      <p class="w-2/3 lg:w-2/6 text-center text-xl lg:text-2xl font-extrabold text-white">
+        Thanks for the challenge... and the banana.
+        “Other reasons” never tasted so fruity
+      </p>
     </div>
   </div>
 </template>
@@ -38,7 +18,7 @@ import ItemCardSkeleton from "@/components/shared/ItemCardSkeleton.vue";
 import GradientWrapper from "@/components/shared/GradientWrapper.vue";
 import Pagination from "@/components/shared/Pagination.vue";
 import NoResult from "@/components/pages/home/NoResult.vue";
-
+import angry from "@/assets/images/angry.png"
 const resetTrigger = ref(false);
 const searchQuery = ref("");
 const currentPage = ref(1);
